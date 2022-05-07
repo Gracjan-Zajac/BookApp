b13d8ea46ff92b94ce3cab318bfe586f63959e2e
Gracjan <gracjan.zajac@hotmail.com>
2022-05-07 19:13:24 +0200

    GoodReads init

--- goodreads.py
+++ goodreads.py
@@ -2,9 +2,16 @@ from selenium.webdriver import Firefox
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 
-
 GOODREADS_FRONTPAGE = 'https://www.goodreads.com/'
 
 
 class GoodReads:
-    pass
+
+    def __init__(self):
+        # Creates headless browser:
+        opts = Options()
+        opts.headless = True
+        assert opts.headless
+        self.browser = Firefox(options=opts)
+        self.browser.get(GOODREADS_FRONTPAGE)
+        self.browser.implicitly_wait(5)
