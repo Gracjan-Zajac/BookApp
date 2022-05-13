8134879a61d518cff744175873c4dcf3493d2048
Gracjan <gracjan.zajac@hotmail.com>
2022-05-13 22:16:35 +0200

    menu opt 4 and 5

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from main import Book, BooksDatabase
+from main import BooksDatabase
 import time
 
 database = BooksDatabase('my_books.json')
@@ -39,7 +39,7 @@ while True:
     elif option == 2:
         print('\nWhat book do you want to add?\n')
         details = database.get_book_details()
-        database.add_new_book(details[0], details[1], details[2], details[3], details[4])
+        database.add_new_book(*details)
         print(f'\n{details[0]} is now added to your list!\n')
 
         time.sleep(3)
@@ -63,12 +63,58 @@ while True:
                     break
             except ValueError:
                 print('Invalid option! Please select a correct number.')
-                
-                
+                time.sleep(3)
+
     elif option == 4:
-        pass
+        while True:
+            print("\nWhich book did you finished reading (to quit press '0')?: \n")
+
+            for count, book in enumerate(database.books):
+                print(f'{count + 1}. {book.title}\n')
+
+            try:
+                user_choice = int(input('Your choice: '))
+                if user_choice == 0:
+                    break
+                elif user_choice != 0:
+                    book = database.books[user_choice - 1]
+                    if book.read:
+                        print(f'You have read {book.title} before.')
+                        time.sleep(3)
+                    else:
+                        book.mark_as_read()
+                        print(f'{book.title} has been marked as read.\n')
+                        time.sleep(3)
+                        break
+            except ValueError:
+                print('Invalid option! Please select a correct number.')
+                time.sleep(3)
+
     elif option == 5:
-        pass
+        while True:
+            print("\nWhich book do you want to mark as unread (to quit press '0')?: \n")
+
+            for count, book in enumerate(database.books):
+                print(f'{count + 1}. {book.title}\n')
+
+            try:
+                user_choice = int(input('Your choice: '))
+                if user_choice == 0:
+                    break
+                elif user_choice != 0:
+                    book = database.books[user_choice - 1]
+                    if book.read:
+                        book.mark_as_unread()
+                        print(f'{book.title} has been marked as unread.\n')
+                        time.sleep(3)
+                        break
+                    else:
+                        print(f'You have not read {book.title} before.')
+                        time.sleep(3)
+            except ValueError:
+                print('Invalid option! Please select a correct number.')
+                time.sleep(3)
+
     elif option == 6:
         pass
     elif option == 7:
